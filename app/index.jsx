0da7338c732ce9f59354489782ec0c191f526cf6
Rica2910 <155787519+Rica2910@users.noreply.github.com>
2025-04-10 15:09:32 +0700

    opening page

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css";
+import CustomButton from "../components/CustomButton";
+import { router } from "expo-router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height:"100%" }}>
+        <View className="items-center justify-center w-full h-full px-4">
+          <Text className="color-black font-bold text-3xl">
+            Membantu pengendalian sampah bersama {' '}
+            <Text className="color-secondary">Re-App</Text>
+          </Text>
+
+          <View className="mt-4 justify-center items-center">
+            <Text className="text-sm font-pregular text-center">
+              Dimana kebersihan dan kenyamanan bertemu: Mari bergabung dalam komunitas yang tak terbatas di {' '}
+              <Text className="color-secondary">Re-App</Text>
+            </Text>
+
+            <CustomButton 
+              title="Continue with Email"
+              handlePress={() => router.push('/sign-in')}
+              containerStyles={'w-full mt-7'}
+            />
+          </View>
+          
+          <StatusBar style="dark" backgroundColor="#fff"/>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./component/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#fff",
+        secondary: {
+          DEFAULT: "#2dcd6e",
+          100: "#d5f5e2",
+          200: "#c0f0d4",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          100: "#CDCDE0",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
